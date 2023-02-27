18aa7f905f5227c294ac37124c4a8dcf91d09426
Elliott Smith <117014763+ElliottSmith610@users.noreply.github.com>
2023-02-27 21:39:30 +0000

    base folder layout

--- my_site/base/views.py
+++ my_site/base/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+rooms = [
+    {'id': 1, 'name': 'Fridays With Pewdiepie'},
+    {'id': 2, 'name': 'I love memes'},
+    {'id': 3, 'name': 'Cor yeah mate'},
+]
+
+def home(request):
+    return redirect('soundboard')
+
+def soundboard(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
