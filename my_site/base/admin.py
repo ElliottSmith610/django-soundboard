72ab4277a51458372e81ccc43fc41ac726ca6927
Elliott Smith <117014763+ElliottSmith610@users.noreply.github.com>
2023-02-27 23:26:14 +0000

    Admin Panel Access to new Database models

--- my_site/base/admin.py
+++ my_site/base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SoundClip, Person, Message
+
+admin.site.register(Person)
+admin.site.register(SoundClip)
+admin.site.register(Message)
